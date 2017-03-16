7947ad01d43a2756919b66b17eca938d0391d39d
Johnny Da Costa <johnny.dacosta@he-arc.ch>
2017-03-16 10:54:20 +0100

    Ajout du fichier de base UseItertools

---
+++ source/itertools/UseIterTools.py
@@ -0,0 +1,13 @@
+#itertools 
+
+from itertools import *
+
+a = [0, 1, 2, 3, 4];
+b = ["zéro", "un", "deux", "trois", "quatre"];
+c = ["a", "b", "c", "d"];
+
+'''function chain()'''
+for i in chain(a, b, c):
+    print(i);
+    
+print(list(chain(a, b, c)));
