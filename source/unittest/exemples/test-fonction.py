98d42226ff6024b7b9cff2c8eeb94ac597682a69
Neto Da Silva André <andre.netodasi@he-arc.ch>
2017-03-19 23:08:53 +0100

    Docstring added

--- source/unittest/exemples/test-fonction.py
+++ source/unittest/exemples/test-fonction.py
@@ -3,14 +3,17 @@ import unittest
 
 # definition
 def carre(x):
+    """Return x**2"""
     return x ** 2
 
 
 # classTest
 class Testeur(unittest.TestCase):
+    """Class with one test"""
     testValues = {2: 4, 0: 0, -2: 4}
 
     def testCarre(self):
+        """Test the function carre"""
         for i, a in self.testValues.items():
             self.assertEqual(carre(i), a)
 
