5cc1a63c59742ec59347de4fc244502bbb42f322
Neto Da Silva André <andre.netodasi@he-arc.ch>
2017-03-19 22:50:48 +0100

    Corrected some bugs of test-fonction.py

--- source/unittest/exemples/test-fonction.py
+++ source/unittest/exemples/test-fonction.py
@@ -1,13 +1,20 @@
 import unittest
+
+
 # definition
 def carre(x):
     return x ** 2
+
+
 # classTest
 class Testeur(unittest.TestCase):
-    testValues = {2:4, 0:0, -2:4}
+    testValues = {2: 4, 0: 0, -2: 4}
+
     def testCarre(self):
         for i, a in self.testValues.items():
-            self.assertEqual(carre(i),a)
-#execution
+            self.assertEqual(carre(i), a)
+
+
+# execution
 if __name__ == '__main__':
     unittest.main()
