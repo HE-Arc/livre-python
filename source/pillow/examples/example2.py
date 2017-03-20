39db7e4ad4238711734c6d0d405b057064424cc6
QVaucher <quentin.vaucher@protonmail.com>
2017-03-20 12:09:48 +0100

    Move color outside the loop in example2.

--- source/pillow/examples/example2.py
+++ source/pillow/examples/example2.py
@@ -11,17 +11,18 @@ img = img.transpose(Image.FLIP_LEFT_RIGHT)
 width = img.size[0]
 height = img.size[1]
 
+white = 250
+purple = (155, 89, 182)
+blue = (52, 152, 219)
+
 for x in range(width):
     for y in range(height):
         pixel = img.getpixel((x, y))
-        white = 250
         # On vérifie que le pixel est blanc
-        if all(chanel > white for chanel in pixel):
+        if all(channel > white for channel in pixel):
             if x + y <= width:
-                purple = (155, 89, 182)
                 img.putpixel((x, y), purple)
             else:
-                blue = (52, 152, 219)
                 img.putpixel((x, y), blue)
 
 img.show()
