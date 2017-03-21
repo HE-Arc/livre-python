7de835ab0d674d6886acc87f9ff7fa10f02ecdb9
Schnaebele Marc <marc.schnaebe@INF15-SCHNAEBEM.intra.eiaj.ch>
2017-03-21 09:03:26 +0100

    third python N corrections

--- source/binary/code.py
+++ source/binary/code.py
@@ -1,22 +1,25 @@
+from collections import namedtuple
+from struct import *
+
 """Module d'exemple pour les types bytes, memoryview et struct."""
 
 
 # Bytes-------------------------------------------------------------------------------
 
-msg= bytes('exemple', encoding= 'utf-8')
+msg = bytes('exemple', encoding='utf-8')
 # où mais l'encodage par défaut sera utilisé.
-msg= b"exemple"
+msg = b"exemple"
 
 b'exemple'
 
 msg[0]
 
 # Cast string en bytes.
-my_str= "exemple"
-bytes=str.encode(my_str)
+my_str = "exemple"
+bytes = str.encode(my_str)
 
 # Cast bytes en string.
-my_decoded_str= str.decode(bytes)
+my_decoded_str = str.decode(bytes)
 type(my_decoded_str)  # ensure it is string representation
 
 i = 16
@@ -24,14 +27,14 @@ i = 16
 # Crée 1 byte avec un int 16.
 # Attention à utiliser le bon encodage (little ou big endian).
 # vérifiez avec sys.byteorder.
-single_byte= i.to_bytes(1, byteorder= 'big', signed= True)
+single_byte = i.to_bytes(1, byteorder='big', signed=True)
 print(single_byte)
 
 # Crée un bytes avec une liste dev int (0-255).
-bytes_from_list= bytes([255, 254, 253, 252])
+bytes_from_list = bytes([255, 254, 253, 252])
 
 # Crée un byte avec un int en base 2.
-one_byte=int('11110000', 2)
+one_byte = int('11110000', 2)
 print(one_byte)
 
 # Print out binary string (e.g. 0b010010).
@@ -39,41 +42,41 @@ print(bin(22))
 
 # Bytes à Integer.
 # Crée un int avec un bytes (non signé par défaut).
-i=int.from_bytes(some_bytes, byteorder= 'big')
+i = int.from_bytes(some_bytes, byteorder='big')
 
 # Crée un int signé.
-i= int.from_bytes(b'\x00\x0F', byteorder= 'big', signed= True)
+i = int.from_bytes(b'\x00\x0F', byteorder='big', signed=True)
 
 # Utilise une liste d'entiers comme source pour le cast.
-i= int.from_bytes([255, 0, 0, 0], byteorder= 'big')
+i = int.from_bytes([255, 0, 0, 0], byteorder='big')
 
 with open("test_file.dat", "rb") as binary_file:
     # Lit tout le fichier.
-    data= binary_file.read()
+    data = binary_file.read()
 print(data)
 
 # Lit N bytes depuis une certaine position.
 binary_file.seek(0)
-couple_bytes= binary_file.read(2)
+couple_bytes = binary_file.read(2)
 print(couple_bytes)
 
 
 # BytesArray--------------------------------------------------------------------
 
 # Crée un bytearray à partir d'un objet bytes.
-msg= bytearray(b"exemple")
+msg = bytearray(b"exemple")
 # Crée un  bytearray à partir d'une chaine de caractères.
-msg= bytearray("exemple", "utf-8")
+msg = bytearray("exemple", "utf-8")
 # Crée un  bytearray à partir d'une liste d'entiers entre 0 et 255.
-msg= bytearray([94, 91, 101, 125, 111, 35, 120, 101, 115, 101, 200])
+msg = bytearray([94, 91, 101, 125, 111, 35, 120, 101, 115, 101, 200])
 
 msg[0]
 
 # Cast bytes à bytearray.
-mutable_bytes=bytearray(b'\x00\x0F')
+mutable_bytes = bytearray(b'\x00\x0F')
 
 # Cast bytearray à bytes.
-immutable_bytes= bytes(mutable_bytes)
+immutable_bytes = bytes(mutable_bytes)
 
 
 # MemoryView-----------------------------------------------------------------------
@@ -91,7 +94,7 @@ PyObject * PyMemoryView_FromBuffer(Py_buffer *view)
 # le pointeur pointe sur cette zone mémoire
 # sinon une copie est faite.
 PyObject * PyMemoryView_GetContiguous
-(PyObject *obj, int buffertype, char order)
+(PyObject * obj, int buffertype, char order)
 
 # Retourne true si l'objet obj est une memoryview.
 int PyMemoryView_Check(PyObject *obj)
@@ -100,14 +103,14 @@ int PyMemoryView_Check(PyObject *obj)
 Py_buffer * PyMemoryView_GET_BUFFER(PyObject *obj)
 
 # mybuf = ...  un grand buffer de bytes.
-mv_mybuf=memoryview(mybuf)  # une memoryview de mybuf.
+mv_mybuf = memoryview(mybuf)  # une memoryview de mybuf.
 func(mv_mybuf[:len(mv_mybuf)//2])
 # passe la première moitié de mybuf dans func comme une "sous-view"
 # crée par le découpage de la memoryview.
 # Aucune copie n'est faite ici!
 
-buf= bytearray(b'abcdefgh')
-mv= memoryview(buf)
+buf = bytearray(b'abcdefgh')
+mv = memoryview(buf)
 mv[4:6]=b'ZA'
 buf
 bytearray(b'abcdZAgh')
@@ -131,16 +134,16 @@ PyObject * PyMemoryView_GetContiguous
 (PyObject *obj, int buffertype, char order)
 
 # packing et unpacking de trois entiers.
-from struct import *
+
 pack('hhl', 1, 2, 3)
 # sortie : '\x00\x01\x00\x02\x00\x00\x00\x03'
 unpack('hhl', '\x00\x01\x00\x02\x00\x00\x00\x03')
 # sortie : (1, 2, 3)
 
 # On peut assigner des noms aux champs.
-record= 'raymond   \x32\x12\x08\x01\x08'
-name, serialnum, school, gradelevel= unpack('<10sHHb', record)
-from collections import namedtuple
-Student= namedtuple('Student', 'name serialnum school gradelevel')
+record = 'raymond   \x32\x12\x08\x01\x08'
+name, serialnum, school, gradelevel = unpack('<10sHHb', record)
+
+Student = namedtuple('Student', 'name serialnum school gradelevel')
 Student._make(unpack('<10sHHb', record))
-Student(name= 'raymond   ', serialnum= 4658, school= 264, gradelevel= 8)
+Student(name = 'raymond   ', serialnum = 4658, school = 264, gradelevel = 8)
