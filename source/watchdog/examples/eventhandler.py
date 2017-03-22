846e4a29f7efbfe2ab82ab1a427fedb95081d6ca
paul.jeanbour <pa.jeanbourquin@gmail.com>
2017-03-22 18:13:06 +0100

    bis 3

---
+++ source/watchdog/examples/eventhandler.py
@@ -0,0 +1,33 @@
+"""Exemple de gestionnaire d'évènements."""
+
+from watchdog.events import FileSystemEventHandler
+from watchdog.events import PatternMatchingEventHandler
+
+class AuditHandler(FileSystemEventHandler):
+    """Gestion d'audit."""
+
+    def on_modified(self, event):
+        """Un fichier a été modifié."""
+        print("Le fichier %s a été modifié" % event.src_path)
+
+    def on_created(self, event):
+        """Un fichier a été créé."""
+        print("Le fichier %s a été créé" % event.src_path)
+
+    def on_deleted(self, event):
+        """Un fichier a été supprimé."""
+        print("Le fichier %s a été supprimé" % event.src_path)
+class AuditHandlerMusic(PatternMatchingEventHandler):
+    """Gestion d'audit."""
+
+    def on_modified(self, event):
+        """Un fichier a été modifié."""
+        print("Le fichier %s a été modifié" % event.src_path)
+
+    def on_created(self, event):
+        """Un fichier a été créé."""
+        print("Le fichier %s a été créé" % event.src_path)
+
+    def on_deleted(self, event):
+        """Un fichier a été supprimé."""
+        print("Le fichier %s a été supprimé" % event.src_path)
