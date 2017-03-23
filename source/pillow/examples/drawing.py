961ba4f31062348ebc8b34bac0740edbb2aeaa47
QVaucher <quentin.vaucher@protonmail.com>
2017-03-23 11:51:55 +0100

    const are now written upper case.

--- source/pillow/examples/drawing.py
+++ source/pillow/examples/drawing.py
@@ -4,28 +4,29 @@ from PIL import Image, ImageDraw
 
 width = 160
 height = 160
-white = (255, 255, 255, 0)
 
-image = Image.new('RGBA', (width, height), white)
+WHITE = (255, 255, 255, 0)
+
+image = Image.new('RGBA', (width, height), WHITE)
 
 # Obtention du contexte graphique
 draw = ImageDraw.Draw(image)
 
-green = (0, 255, 0, 0)
+GREEN = (0, 255, 0, 0)
 draw.line((0, height//2,
           width, height//2),
-          fill=green)
+          fill=GREEN)
 draw.line((width//2, 0,
           width//2, height),
-          fill=green)
+          fill=GREEN)
 
-blue = (0, 0, 255, 0)
+BLUE = (0, 0, 255, 0)
 draw.ellipse((width//4, height//4,
              3*width//4, 3*height//4),
-             outline=blue)
+             outline=BLUE)
 
-black = (0, 0, 0, 0)
-draw.text((20, 20), "Cercle trigonométrique", fill=black)
+BLACK = (0, 0, 0, 0)
+draw.text((20, 20), "Cercle trigonométrique", fill=BLACK)
 
 image.show()
 image.save("cercleTrigo.png", "png")
