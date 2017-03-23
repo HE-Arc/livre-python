3795cf94fe77aef6253491753ed0e77ab302899f
Christophe Hirschi <christophe.hirschi@he-arc.ch>
2017-03-23 14:12:49 +0100

    Fix exemple

--- source/random/examples/random_seeding.py
+++ source/random/examples/random_seeding.py
@@ -5,7 +5,7 @@ import random
 # Attribution d'une graine.
 random.seed(1)
 
-# Retour de valeurs
-print(random.random()) # N°1
-print(random.random()) # N°2
-print(random.random()) # N°3
+# Retour de valeurs.
+print(random.random())  # N°1
+print(random.random())  # N°2
+print(random.random())  # N°3
