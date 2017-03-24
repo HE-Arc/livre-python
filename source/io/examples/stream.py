a75ff005ff0c3cdffa8736a9e1ecc662dde15534
Dylan Santos de Pinho <dylan.santosdepinho@he-arc.ch>
2017-03-24 12:09:36 +0100

    Rebase

--- source/io/examples/stream.py
+++ source/io/examples/stream.py
@@ -1,10 +1,5 @@
 """Exemple de lecture et écriture avec io."""
 
-try:
-    with open('input.txt', 'r') as inputStream:
-        with open('output.txt', 'w') as os:
-            os.write(inputStream.read(10))
-except PermissionError:
-    print("PermissionError")
-except OSError:
-    print("OSError/IOError")
+with open('input.txt', 'r') as inputStream:
+    with open('output.txt', 'w') as os:
+        os.write(inputStream.read(10))
