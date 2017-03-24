a4098a68b80ffcfe109a60d5657dd9928fc49391
marc.friedli <marc.friedli@he-arc.ch>
2017-03-24 16:25:11 +0100

    modification des fichiers python

--- source/dunders/someclass.py
+++ source/dunders/someclass.py
@@ -1,14 +1,15 @@
-class SomeClass(object) :
-    grammar ={
-        1 : "Some string",
-        2 : "Another string",
-        3 : 42,
-        4 : True
+class SomeClass(object):
+    grammar = {
+        1: "Some string",
+        2: "Another string",
+        3: 42,
+        4: True
     }
 
-    def __init__(self, name) :
-        self.name=name
+    def __init__(self, name):
+        self.name = name
+
 
 sc = SomeClass("Totoro")
-print (len(sc.name)) #affiche la taille du string (6)
-print (len(sc.grammar)) #affiche le nombre d'éléments (4)
+print(len(sc.name))  # affiche la taille du string (6)
+print(len(sc.grammar))  # affiche le nombre d'éléments (4)
--- source/dunders/vector.py
+++ source/dunders/vector.py
@@ -1,29 +1,31 @@
 from math import hypot
 
+
 class Vector:
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
-        
+
     def __repr__(self):
         return 'Vecteur(%r, %r)' % (self.x, self.y)
-    
+
     def __abs__(self):
-         return hypot(self.x, self.y)
-    
+        return hypot(self.x, self.y)
+
     def __add__(self, other):
         x = self.x + other.x
         y = self.y + other.y
         return Vector(x, y)
-    
+
     def __mul__(self, scalar):
         return Vector(self.x * scalar, self.y * scalar)
 
     def __truediv__(self, other):
         raise TypeError("On ne peut pas diviser un vecteur !")
 
-vect = Vector(4,7)
-print(repr(vect)+" a pour norme "+str(abs(vect)))
-vect = vect*2 #(8,14)
-vect+=Vector(2,5) #(10,19)
-vect/=Vector(4,5) #lève une erreur
+
+vect = Vector(4, 7)
+print(repr(vect) + " a pour norme " + str(abs(vect)))
+vect *= 2  # (8,14)
+vect += Vector(2, 5)  # (10,19)
+vect /= Vector(4, 5)  # lève une erreur
