3a3876f859c4ea1bd137c7be42d7c6f818472d8b
Schnaebele Marc <marc.schnaebe@INF15-SCHNAEBEM.intra.eiaj.ch>
2017-06-01 10:09:10 +0200

    corrections merge exemple

--- source/binary/examples/memoryview_ex_python.py
+++ source/binary/examples/memoryview_ex_python.py
@@ -1,22 +1,9 @@
 """Module d'exemple d'une memoryview."""
 
-<<<<<<< 8c201756bfefb3a8877d52786a0d8290c49e8c31
-with open('../../_static/json.png', 'rb') as f:
-    mybuf = f.read()
-
-
-def func(data):
-    """Traite des données..."""
-
-
-mv_mybuf = memoryview(mybuf)  # une memoryview de mybuf.
-func(mv_mybuf[:len(mv_mybuf) // 2])
-=======
 
 # mybuf = ...  un grand buffer de bytes.
 mv_mybuf = memoryview(mybuf)  # une memoryview de mybuf.
 func(mv_mybuf[:len(mv_mybuf)//2])
->>>>>>> new branch
 # passe la première moitié de mybuf dans func comme une "sous-view"
 # créé par le découpage de la memoryview.
 # Aucune copie n'est faite ici!
