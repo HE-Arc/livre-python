85dd7ed6e7b43264785e6bcf3a30f46b209e9ff4
Griesser Gabriel <gabriel.griesser@he-arc.ch>
2017-06-08 09:41:39 +0200

    Rechange carreé.py

--- source/unittest/examples/carre.py
+++ source/unittest/examples/carre.py
@@ -1,39 +1,39 @@
-"""Exemple simple de test unitaire sur une fonction carre."""
+"""Exemple simple de test unitaire sur une fonction carré."""
 
 import unittest
 
 
-# func:carre
-def carre(x):
-    """Élève au carre."""
+# func:carré
+def carré(x):
+    """Élève au carré."""
     return x ** 2
 
 
-# endfunc:carre
+# endfunc:carré
 
 
 # class:TestCase
 class CarreTestCase(unittest.TestCase):
-    """Classe testeur pour la fonction carre."""
+    """Classe testeur pour la fonction carré."""
 
     test_values = ((2, 4), (0, 0), (-2, 4))
 
-    def test_carre(self):
+    def test_carré(self):
         """Teste les valeurs références."""
         for value, expected in self.test_values:
-            self.assertEqual(expected, carre(value))
+            self.assertEqual(expected, carré(value))
 
 
 # endclass:TestCase
 
 # class:CarreTestCaseFail
 class CarreTestCaseFail(unittest.TestCase):
-    """Mauvaise classe testeur pour la fonction carre."""
+    """Mauvaise classe testeur pour la fonction carré."""
 
     def test_assert_fail(self):
         """Affichage d'un message d'erreur personnalisé."""
         value = 2
-        self.assertEqual(2, carre(value), "Message d'erreur personnalisé")
+        self.assertEqual(2, carré(value), "Message d'erreur personnalisé")
 
 
 # endclass:CarreTestCaseFail
