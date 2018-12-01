e8f58cb5c7055671121c647984856da4556c7cd8
Yoan Blanc <yoan@dosimple.ch>
2018-12-01 10:12:44 +0100

    pbr: adapt to upstream changes
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- getPBR.sh
+++ getPBR.sh
@@ -8,13 +8,12 @@ rm -f index.rst
 
 pbr_dir="Publishing-to-PyPI-with-pbr-and-Travis"
 git clone https://github.com/73VW/${pbr_dir}.git
-mv $pbr_dir/*.rst .
-mv $pbr_dir/*.PNG ../_static
-sed -E -i "s/\.\. image:: (.*\.*)/.. image:: ..\/_static\/\1/g" README.rst
+cp $pbr_dir/docs/main.rst index.rst
+cp $pbr_dir/docs/_static/*.PNG ../_static
+sed -E -i "s/\.\. image:: /.. image:: ..\//g" index.rst
 rm -rf $pbr_dir
 
-for f in note.rst README.rst; do (cat "${f}"; echo) >> index.rst; done
+(cat note.rst; echo) >> index.rst
 echo '.. _`Maël Pedretti`: https://github.com/73VW/Publishing-to-PyPI-with-pbr-and-Travis' >> index.rst
 rm -f note.rst
-rm -f README.rst
 cd ../..
